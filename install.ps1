73119344189547ee9d2ec694d6cd012f89f44fc2
Vince <98156995+hellsuck@users.noreply.github.com>
2024-12-30 01:22:52 +0100

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,4 +1,4 @@
-# Make sure the user is in their own home directory and **NOT** System32.
+# Ensure user is in their home directory
 $homeDirectory = [Environment]::GetFolderPath("UserProfile")
 Set-Location -Path $homeDirectory
 
@@ -6,24 +6,24 @@ Set-Location -Path $homeDirectory
 winget install -e --id OpenJS.NodeJS
 winget install -e --id Git.Git
 
-# Install pnpm using npm
+# Install pnpm using npm and update PATH
 powershell -ExecutionPolicy Bypass -Command "& { npm install -g pnpm@9.1.0 }"
-
+$env:Path += ";" + (npm prefix -g | ForEach-Object { "$_\node_modules\.bin" })
+pnpm --version  # Ensure pnpm is installed correctly
 
 # Clone the Vencord repository
 git clone https://github.com/hellsuck/vencordradiant.git
 
-# Download a file into the cloned repository directory
-
-# Kill all Discord clients including Canary and PTB versions. This is necessary to prevent the installer from failing.
+# Kill all Discord clients to prevent installer issues
 Stop-Process -Name Discord -Force -ErrorAction SilentlyContinue
 Stop-Process -Name DiscordCanary -Force -ErrorAction SilentlyContinue
 Stop-Process -Name DiscordPTB -Force -ErrorAction SilentlyContinue
+
+# Navigate to the cloned repository directory and run the installer
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
-# Run the installer for Vencord
 Set-Location vencordradiant\
 powershell -ExecutionPolicy Bypass -Command "& { pnpm install; pnpm run build; pnpm run inject }"
 
-# Prompt installer should have completed successfully assuming you followed the instructions.
+# Final output messages
 Write-Output "If you followed the instructions correctly, the installer should have completed successfully."
 Write-Output "You'll need to make sure to enable the plugin (Radiant Tweaks) in the Vencord settings inside of Discord > Settings > Plugins > Radiant Tweaks and turn the switch on and click restart."
